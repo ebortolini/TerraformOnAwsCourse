9b66ff5f420f0e87c20c227ad3a4d619d7efc3ae
Eduardo Bortolini <ebortolini@gmail.com>
2026-04-26 20:22:03 -0300

    Utility project basic version

---
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-03-Utility-Project/terraform-manifests/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
@@ -0,0 +1,15 @@
+data "aws_ec2_instance_type_offerings" "my_inst_type1" {
+    filter {
+        name = "instance-type"
+        values = ["t3.micro"]
+    }
+    filter {
+        name = "location"
+        values = ["us=east-1a"]
+    }
+    location_type = "availability-zone"
+}
+
+output "output_v1_1" {
+  value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
+}
